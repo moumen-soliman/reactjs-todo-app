0a15be9ddc15cdb4a4c0dd10544bbcdfc8f9e19d
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-15 15:04:50 +0200

    add header component

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+      return (
+        <div>
+          <h1>ToDo</h1>
+          <h2>Put your life in the hands of a computer</h2>
+        </div>
+      )
+      
+    }
+}
+
+export default Header;
+  
