7fbcb1fc8f015c2efebc288791b80216a6070467
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-15 20:35:36 +0200

    create counter app by state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 //Components
 import Header from './components/main/header.js'
+import Counter from './components/main/counter.js'
 import Action from './components/actions/action.js'
 // import Option from './components/options/option.js'
 import Options from './components/options/options.js'
@@ -11,12 +12,14 @@ import AddOptions from './components/options/addoptions.js'
 class App extends Component {
   render() {
     const option = ['Thing one', 'Thing two', 'Thing four'];
+    const number = 0;
     return (
       <div className="container">
         <Header />
         <Action />
         <Options option={option}/>
         <AddOptions />
+        <Counter />
       </div>
     );
   }
---
+++ src/components/main/counter.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+
+    handleAddOne() {
+        this.setState((prevStateAdd) => {
+            return {
+                count: prevStateAdd.count + 1
+            };
+        });
+    }
+
+    handleMinusOne() {
+        this.setState((prevStateMinus) => {
+            return {
+                count: prevStateMinus.count - 1
+            };
+        });
+    }
+
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
+    }
+}
+
+//handleAddOne, handleMinusOne, handleReset
+
+export default Counter;
